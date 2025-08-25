ff6648e7698d9c6bc8bd5acc39d026964010f2ec
Garry Hill <garry@magnetised.net>
2025-08-25 13:51:33 +0100

    first version

---
+++ shell.nix
@@ -0,0 +1,169 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+# https://nixos.org/manual/nixpkgs/stable/#beam-introduction
+# https://github.com/NixOS/nixpkgs/blob/master/pkgs/top-level/beam-packages.nix
+# https://medium.com/@ejpcmac/using-nix-in-elixir-projects-ff5300214e70
+# https://ejpcmac.net/blog/using-nix-in-elixir-projects/
+with pkgs;
+let
+  nur = pkgs.callPackage (import (
+    builtins.fetchGit {
+      url = "https://github.com/nix-community/NUR";
+    }
+  )) { };
+
+  erlang = beam.interpreters.erlang_27;
+  elixir = beam.packages.erlang_27.elixir_1_18;
+
+  elixir-ls = beam.packages.erlang_27.elixir-ls;
+
+  # this is how to use a gh version of elixir
+  _elixir_main = beam.packages.erlang_27.elixir.overrideAttrs (old: {
+    name = "elixir-1.19-pre";
+    src = fetchFromGitHub {
+      rev = "699bb9112e2430a5f1fd965a2a5a0312844a541f";
+      sha256 = "sha256-pdo5t+ZbIZo3287d79U3zqqj46bqXdjpwkvTHllKch0=";
+      owner = "elixir-lang";
+      repo = "elixir";
+    };
+  });
+
+  luxVersion = "2.1";
+  lux = stdenv.mkDerivation {
+    name = "lux-${luxVersion}";
+    nativeBuildInputs = [
+      autoconf
+      erlang
+    ];
+    src = fetchFromGitHub {
+      # owner = "hawk";
+      # repo = "lux";
+      owner = "electric-sql";
+      repo = "lux";
+      rev = "otp-27";
+      hash = "sha256-37KxPVXcT6eRPP16R2HR31aitilb0XSO3uzrtWTwVeA=";
+    };
+
+    preConfigurePhases = [ "autoconf" ];
+  };
+  nodejs-pinned = (
+    (import (builtins.fetchGit {
+      # https://lazamar.co.uk/nix-versions/?channel=nixpkgs-unstable&package=nodejs
+      name = "nixpkgs-nodejs-20.18.1"; # name in nix store
+      url = "https://github.com/NixOS/nixpkgs/";
+      ref = "refs/heads/nixpkgs-unstable";
+      rev = "4f0dadbf38ee4cf4cc38cbc232b7708fddf965bc";
+    }) { }).nodejs_20
+  );
+
+  crushConfig = pkgs.writeText ".crush.json" (
+    builtins.toJSON {
+      "$schema" = "https://charm.land/crush.json";
+      lsp = {
+        elixir = {
+          command = "elixir-ls";
+        };
+      };
+    }
+  );
+
+in
+# next-ls =
+#   let
+#     packages = beam.packagesWith erlang;
+#     pname = "next-ls";
+#     version = "0.23.0";
+#
+#     src = fetchFromGitHub {
+#       owner = "elixir-tools";
+#       repo = "next-ls";
+#       rev = "v${version}";
+#       hash = "sha256-wTEf0pxVIT7qmPufAN9vGR9rY31kWjNabYZwKe/hkVU=";
+#     };
+#     mixFodDeps = beamPackages.fetchMixDeps {
+#       pname = "${pname}-deps";
+#       inherit src version elixir;
+#       hash = "sha256-4Rt5Q0fX+fbncvxyXdpIhgEvn9VYX/QDxDdnbanT21Q=";
+#       mixEnv = "prod";
+#     };
+#   in
+#   packages.mixRelease {
+#     buildInputs = [
+#       pkgs.zig_0_11
+#       pkgs.xz
+#     ];
+#     # doesn't work because elixir_make fails with read-only build root
+#     inherit
+#       erlang
+#       elixir
+#       src
+#       pname
+#       version
+#       mixFodDeps
+#       ;
+#     mixEnv = "prod";
+#     removeCookie = false;
+#     installPhase = ''
+#       mix release --no-deps-check --path $out plain
+#       echo "$out/bin/plain eval \"System.no_halt(true); Application.ensure_all_started(:next_ls)\" \"\$@\"" > "$out/bin/nextls"
+#       chmod +x "$out/bin/nextls"
+#     '';
+#   };
+mkShell {
+  # buildInputs = [ pkgs.zig pkgs.xz ];
+  packages = [
+    lux
+    erlang
+    # elixir_main
+    elixir
+    elixir-ls
+    # next-ls
+    postgresql_17
+    nodejs-pinned
+    nodejs-pinned.pkgs.pnpm
+    nodejs-pinned.pkgs.node-gyp
+    # for clang-format for protobuf
+    clang
+    gcc
+    autoconf
+    automake
+    prisma-engines
+    # keep in sync with version installed by ci in
+    # .github/workflows/satellite_proto.yml
+    protobuf_26
+    bun
+    inotify-tools
+    # next-ls
+    ngrok
+    cloudflared
+    flyctl
+    varnish
+    awscli2
+    nginx
+    libtool
+    pkg-config
+    rustfmt
+    cargo
+    libcgroup
+    nur.repos.charmbracelet.crush
+    alsa-lib
+    alsa-utils
+    alsa-tools
+  ];
+  # https://nixos.wiki/wiki/Development_environment_with_nix-shell
+  # NIX_ENFORCE_PURITY lets me cc things
+  shellHook = ''
+    # export PATH="$PWD/node_modules/.bin/:$PATH"
+
+    # exec env NIX_ENFORCE_PURITY=0 fish -c 'exec tmux -L $(basename $(pwd)) new -A'
+  '';
+  PRISMA_MIGRATION_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/migration-engine";
+  PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
+  PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
+  PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
+  PRISMA_INTROSPECTION_ENGINE_BINARY = "${prisma-engines}/bin/introspection-engine";
+  PRISMA_FMT_BINARY = "${pkgs.prisma-engines}/bin/prisma-fmt";
+  GEMINI_API_KEY = "AIzaSyAXGmw12owFCCwmvBr-RzsE3eUuMb5Px_A";
+
+}
---
+++ src/main.rs
@@ -0,0 +1,127 @@
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use rustfft::num_complex::Complex;
+use rustfft::FftPlanner;
+use std::sync::{mpsc, Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+
+const NUM_BINS: usize = 128;
+const RESOLUTION: usize = 4096;
+const MIN_FREQ: f32 = 1000.0;
+const MAX_FREQ: f32 = 4000.0;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (_stream, rx) = setup_audio_capture()?;
+    let bins = Arc::new(Mutex::new(vec![0.0; NUM_BINS]));
+
+    let bins_clone = Arc::clone(&bins);
+    thread::spawn(move || {
+        let mut planner = FftPlanner::new();
+        let fft = planner.plan_fft_forward(RESOLUTION);
+        loop {
+            match rx.recv() {
+                Ok(buffer) => {
+                    let magnitudes = process_fft(buffer, &fft);
+                    let new_bins = bin_magnitudes(magnitudes);
+                    if let Ok(mut bins_lock) = bins_clone.lock() {
+                        *bins_lock = new_bins;
+                    }
+                }
+                Err(_) => break,
+            }
+        }
+    });
+
+    let mut peak_magnitudes = vec![0.0; NUM_BINS];
+    loop {
+        thread::sleep(Duration::from_millis(16));
+        if let Ok(bins_lock) = bins.lock() {
+            visualize_bins(bins_lock.clone(), &mut peak_magnitudes);
+        }
+    }
+}
+
+fn setup_audio_capture() -> Result<(cpal::Stream, mpsc::Receiver<Vec<Complex<f32>>>), Box<dyn std::error::Error>> {
+    let host = cpal::default_host();
+    let device = host.default_input_device().expect("no input device available");
+    let config = device.default_input_config().expect("no default input config");
+    let (tx, rx) = mpsc::channel();
+
+    let stream = device.build_input_stream(
+        &config.into(),
+        move |data: &[f32], _: &cpal::InputCallbackInfo| {
+            let buffer: Vec<Complex<f32>> = data.iter().map(|&s| Complex::new(s, 0.0)).collect();
+            if tx.send(buffer).is_err() {
+                // The receiver has been dropped, so we can stop the thread.
+            }
+        },
+        |err| eprintln!("an error occurred on stream: {}", err),
+        None,
+    )?;
+
+    stream.play()?;
+    Ok((stream, rx))
+}
+
+fn process_fft(mut buffer: Vec<Complex<f32>>, fft: &std::sync::Arc<dyn rustfft::Fft<f32>>) -> Vec<f32> {
+    if buffer.len() > RESOLUTION {
+        buffer.resize(RESOLUTION, Complex::new(0.0, 0.0));
+    } else {
+        buffer.resize(RESOLUTION, Complex::new(0.0, 0.0));
+    }
+    fft.process(&mut buffer);
+    buffer.iter().take(buffer.len() / 2).map(|c| (c.re.powi(2) + c.im.powi(2)).sqrt()).collect()
+}
+
+fn bin_magnitudes(magnitudes: Vec<f32>) -> Vec<f32> {
+    let mut bins = vec![0.0; NUM_BINS];
+    let mut counts = vec![0; NUM_BINS];
+    let frequency_resolution = 44100.0 / RESOLUTION as f32;
+
+    for (i, &magnitude) in magnitudes.iter().enumerate() {
+        let freq = i as f32 * frequency_resolution;
+        if !(MIN_FREQ..=MAX_FREQ).contains(&freq) {
+            continue;
+        }
+        let bin_index = ((freq - MIN_FREQ) / (MAX_FREQ - MIN_FREQ) * NUM_BINS as f32) as usize;
+        if bin_index < NUM_BINS {
+            bins[bin_index] += magnitude;
+            counts[bin_index] += 1;
+        }
+    }
+
+    for i in 0..NUM_BINS {
+        if counts[i] > 0 {
+            bins[i] /= counts[i] as f32;
+        }
+    }
+    bins
+}
+
+fn visualize_bins(bins: Vec<f32>, peak_magnitudes: &mut Vec<f32>) {
+    let mut lines: Vec<String> = Vec::with_capacity(NUM_BINS);
+
+    for (i, &magnitude) in bins.iter().enumerate() {
+        let mut mag = magnitude;
+        const THRESHOLD: f32 = 1.0;
+
+        if mag < THRESHOLD {
+            mag = 0.0;
+        }
+
+        if mag > peak_magnitudes[i] {
+            peak_magnitudes[i] = mag;
+        } else {
+            peak_magnitudes[i] *= 0.6;
+        }
+
+        let brightness = (peak_magnitudes[i] * 255.0).min(255.0) as u8;
+        let character = "■";
+
+        lines.push(format!(
+            "\x1B[38;2;{0};{0};0m{1}\x1B[0m",
+            brightness, character
+        ));
+    }
+    print!("\x1B[2J\x1B[1;1H{}", lines.join(""));
+}
