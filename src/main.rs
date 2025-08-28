73f12dc9d6c4aeb847687ce8749fde5cbcf06dcf
Garry Hill <garry@magnetised.net>
2025-08-28 17:10:11 +0100

    try only re-calculating spectrum on update

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use spectrum_analyzer::scaling::divide_by_N_sqrt;
 use spectrum_analyzer::windows::hann_window;
 use spectrum_analyzer::{samples_fft_to_spectrum, FrequencyLimit, FrequencySpectrum};
 
+use ringbuf::{traits::*, HeapRb};
+
 // https://github.com/RustAudio/cpal/issues/902
 // https://docs.rs/rtrb/latest/rtrb/
 
@@ -16,54 +18,21 @@ const MAX_FREQ: f32 = 4200.0;
 const FADE: f32 = 0.98;
 
 const SAMPLE_SIZE: usize = 8192;
-const RINGBUFFER_SIZE: usize = SAMPLE_SIZE * 8;
+const RINGBUFFER_SIZE: usize = SAMPLE_SIZE;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let (producer, mut consumer) = rtrb::RingBuffer::<f32>::new(RINGBUFFER_SIZE);
-    let (_stream, rx) = setup_audio_capture(producer)?;
-    let bins = Arc::new(Mutex::new(vec![0.0; NUM_BINS]));
+    let ringbuf = ringbuf::HeapRb::<f32>::new(RINGBUFFER_SIZE);
+    // let (_producer, mut consumer) = ringbuf.split();
 
-    let bins_clone = Arc::clone(&bins);
+    // let (producer, mut consumer) = rtrb::RingBuffer::<f32>::new(RINGBUFFER_SIZE);
 
-    thread::spawn(move || loop {
-        match rx.recv() {
-            Ok(_count) => {
-                if consumer.slots() >= SAMPLE_SIZE {
-                    let read_chunk = consumer.read_chunk(SAMPLE_SIZE).unwrap();
-                    let samples = read_chunk.into_iter().collect::<Vec<f32>>();
-                    let hann_window = hann_window(&samples);
-                    let spectrum = samples_fft_to_spectrum(
-                        &hann_window,
-                        44100,
-                        FrequencyLimit::Range(MIN_FREQ, MAX_FREQ),
-                        Some(&divide_by_N_sqrt),
-                    )
-                    .unwrap();
-                    let new_bins = bin_magnitudes(spectrum);
-                    if let Ok(mut bins_lock) = bins_clone.lock() {
-                        *bins_lock = new_bins;
-                    }
-                }
-            }
-            Err(_) => break,
-        }
-    });
+    // let (_stream, rx) = setup_audio_capture(producer)?;
+    let shared_buffer = Arc::new(Mutex::new(ringbuf));
 
-    let mut peak_magnitudes = vec![0.0; NUM_BINS];
+    let producer_buffer = Arc::clone(&shared_buffer);
+    let consumer_buffer = Arc::clone(&shared_buffer);
+    // let bins_clone = Arc::clone(&bins);
 
-    loop {
-        // turn off the cursor
-        print!("\x1B[?25l");
-        if let Ok(bins_lock) = bins.lock() {
-            visualize_bins(bins_lock.clone(), &mut peak_magnitudes);
-        }
-        thread::sleep(Duration::from_millis(4));
-    }
-}
-
-fn setup_audio_capture(
-    mut producer: rtrb::Producer<f32>,
-) -> Result<(cpal::Stream, mpsc::Receiver<usize>), Box<dyn std::error::Error>> {
     let host = cpal::default_host();
     let device = host
         .default_input_device()
@@ -72,28 +41,108 @@ fn setup_audio_capture(
         .default_input_config()
         .expect("no default input config");
 
-    let (tx, rx) = mpsc::channel();
+    // let (tx, rx) = mpsc::channel();
 
     let mut stream_config: cpal::StreamConfig = config.into();
-    stream_config.buffer_size = cpal::BufferSize::Fixed(SAMPLE_SIZE as u32);
+
+    stream_config.buffer_size = cpal::BufferSize::Fixed(256 as u32);
+
+    let my_mutex = Mutex::new(5);
+    let mut peak_magnitudes = vec![0.0; NUM_BINS];
 
     let stream = device.build_input_stream(
         &stream_config,
         move |samples: &[f32], _: &cpal::InputCallbackInfo| {
-            let chunk = producer.write_chunk_uninit(samples.len()).unwrap();
-            chunk.fill_from_iter(samples.iter().copied());
-            if tx.send(samples.len()).is_err() {
-                // The receiver has been dropped, so we can stop the thread.
-                eprintln!("error");
+            if let Ok(mut buffer) = producer_buffer.lock() {
+                buffer.push_iter_overwrite(&mut samples.iter().copied());
             }
+            // let chunk = producer.write_chunk_uninit(samples.len()).unwrap();
+            // chunk.fill_from_iter(samples.iter().copied());
+            // if tx.send(samples.len()).is_err() {
+            //     // The receiver has been dropped, so we can stop the thread.
+            //     eprintln!("error");
+            // }
+            // thread::sleep(Duration::from_millis(6));
         },
         |err| eprintln!("an error occurred on stream: {}", err),
         None,
     )?;
 
     stream.play()?;
-    Ok((stream, rx))
+
+    // thread::spawn(move || {
+    loop {
+        thread::sleep(Duration::from_millis(16));
+        if let Ok(mut buffer) = consumer_buffer.lock() {
+            // if let Ok(lock) = my_mutex.lock() {
+            if buffer.is_full() {
+                let mut samples = [0.0f32; SAMPLE_SIZE];
+                // let read_chunk = consumer.read_chunk(SAMPLE_SIZE).unwrap();
+                let bytes_read = buffer.pop_slice(&mut samples);
+                // let samples = read_chunk.into_iter().collect::<Vec<f32>>();
+                let hann_window = hann_window(&samples);
+                let spectrum = samples_fft_to_spectrum(
+                    &hann_window,
+                    44100,
+                    FrequencyLimit::Range(MIN_FREQ, MAX_FREQ),
+                    Some(&divide_by_N_sqrt),
+                )
+                .unwrap();
+                let new_bins = bin_magnitudes(spectrum);
+                visualize_bins(new_bins, &mut peak_magnitudes);
+                // if let Ok(mut bins_lock) = bins_clone.lock() {
+                //     *bins_lock = new_bins;
+                // }
+            }
+        }
+    }
+    // });
+
+    // loop {
+    //     // turn off the cursor
+    //     print!("\x1B[?25l");
+    //     if let Ok(bins_lock) = bins.lock() {
+    //         visualize_bins(bins_lock.clone(), &mut peak_magnitudes);
+    //     }
+    //     thread::sleep(Duration::from_millis(6));
+    // }
 }
+
+// fn setup_audio_capture(
+//     mut producer: ringbuf::Caching<Arc<SharedRb<Heap<f32>>>, true, false>,
+// ) -> Result<(cpal::Stream, mpsc::Receiver<usize>), Box<dyn std::error::Error>> {
+//     let host = cpal::default_host();
+//     let device = host
+//         .default_input_device()
+//         .expect("no input device available");
+//     let config = device
+//         .default_input_config()
+//         .expect("no default input config");
+//
+//     let (tx, rx) = mpsc::channel();
+//
+//     let mut stream_config: cpal::StreamConfig = config.into();
+//     stream_config.buffer_size = cpal::BufferSize::Fixed(256 as u32);
+//
+//     let stream = device.build_input_stream(
+//         &stream_config,
+//         move |samples: &[f32], _: &cpal::InputCallbackInfo| {
+//             producer.push_iter_overwrite(&mut samples.iter().copied());
+//             // let chunk = producer.write_chunk_uninit(samples.len()).unwrap();
+//             // chunk.fill_from_iter(samples.iter().copied());
+//             if tx.send(samples.len()).is_err() {
+//                 // The receiver has been dropped, so we can stop the thread.
+//                 eprintln!("error");
+//             }
+//             // thread::sleep(Duration::from_millis(6));
+//         },
+//         |err| eprintln!("an error occurred on stream: {}", err),
+//         None,
+//     )?;
+//
+//     stream.play()?;
+//     Ok((stream, rx))
+// }
 
 fn bin_magnitudes(spectrum: FrequencySpectrum) -> Vec<f32> {
     let mut bins = vec![0.0; NUM_BINS];
