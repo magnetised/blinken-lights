6b329cc817ae9a9f7a5ae2f759c91b968a03e9e8
Garry Hill <garry@magnetised.net>
2025-10-11 11:47:57 +0100

    write state to disk

--- lib/blinken_lights/application.ex
+++ lib/blinken_lights/application.ex
@@ -14,7 +14,8 @@ defmodule BlinkenLights.Application do
       {BlinkenLights.DisplayConfig, %BlinkenLights.DisplayConfig{}},
       BlinkenLights.Capture,
       {BlinkenLights.DarkMode,
-       start_time: ~T[21:00:00], end_time: ~T[08:00:00], dark_target: [brightness: 0.05]}
+       start_time: ~T[21:00:00], end_time: ~T[08:00:00], dark_target: [brightness: 0.05]},
+      BlinkenLights.State
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
